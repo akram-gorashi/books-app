a846be87b3db58d6b7fc830b8b37360ec64eca2f
Akram Gorashi <akramgorashi6@gmail.com>
2022-05-03 04:07:39 +0400

    [update] completed adding new book form logic

--- src/app/components/add-book/add-book.component.ts
+++ src/app/components/add-book/add-book.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Book } from 'src/app/models/book';
+import { BooksService } from 'src/app/services/books/books.service';
+import * as uuid from 'uuid';
 
 @Component({
   selector: 'app-add-book',
@@ -7,9 +11,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddBookComponent implements OnInit {
 
-  constructor() { }
+  createBookFormGroup!: FormGroup;
+  bookId = uuid.v4();
+
+  constructor(private booksService: BooksService, private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.initCreateBookForm();
   }
 
+  initCreateBookForm() {
+    this.createBookFormGroup = this.formBuilder.group({
+      title: [null, [Validators.required]],
+      imageUrl: [null, [Validators.required]],
+      year: [null, [Validators.required]],
+      authorName: [null, [Validators.required]],
+    })
+    this.submitCreateBook();
+  }
+  get bookFormCtrl() {
+    return this.createBookFormGroup.controls;
+  }
+  submitCreateBook() {
+    // mocking submit request
+
+    let tempBook: Book = Object.assign({}, {
+      id: this.bookId,
+      title: "Ender's Game",
+      imageUrl: "",
+      year: 1985,
+      authorName: "Orson Scott Card"
+    })
+    this.booksService.addBook(tempBook).subscribe(res => {
+      console.log('success')
+    })
+  }
 }
